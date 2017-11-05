db759356d5ae2c949b0c141ef184a1fd659d2985
Hansy <hansyo.souti.1999@gmail.com>
2017-11-05 21:03:41 +0900

    Update 08_Codic.el
    軽微だがわりかし重要な修正

--- inits/08_Codic.el
+++ inits/08_Codic.el
@@ -1,2 +1,2 @@
-(add-to-list 'load-path "/.emacs.d/elisp/codic-20150926.427")
-(require 'codic)
+(add-to-list 'load-path "~/.emacs.d/elisp/codic-20150926.427")
+(require 'codic"~/.emacs.d/elisp/codic-20150926.427/codic")
